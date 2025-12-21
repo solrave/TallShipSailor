5dcfa5b4945d38444bb600a07f0ee9ba7302bc9e
solrave <demidovsergey777@gmail.com>
2025-12-21 10:50:11 +0100

    3

--- Assets/Scripts/WalletManager.cs
+++ Assets/Scripts/WalletManager.cs
@@ -1,69 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class WalletManager
+
+public class WalletManager : MonoBehaviour
 {
-    private List<Currency> _wallet = new();
-
-    public WalletManager()
+    [SerializeField] private ScrollView _view;
+    [SerializeField] private List<CurrencyViewData> _viewData;
+    private void Awake()
     {
-        _wallet.Add(new Coin());
-        _wallet.Add(new Coin());
-        _wallet.Add(new Coin());
-        _wallet.Add(new Star());
-        _wallet.Add(new Star());
-        _wallet.Add(new Star());
-        _wallet.Add(new Energy());
-    }
-
-    public event Action<int> CurrencyAdded;
-}
-
-public enum CurrencyType
-{
-    Coin, Star, Energy
-}
-
-public abstract class Currency
-{
-    public string Name { get; protected set; }
-    // private CurrencyType _type;
-    //
-    // protected Currency(CurrencyType type)
-    // {
-    //     _type = type;
-    // }
-    //
-    // public string Name => nameof(_type);
-}
-
-public class Coin : Currency
-{
-    public Coin()
-    {
-        Name = "Coin";
+        
     }
 }
 
-public class Star : Currency
+[Serializable]
+public class CurrencyViewData
 {
-    public Star()
-    {
-        Name = "Star";
-    }
-}
-
-public class Energy : Currency
-{
-    public Energy()
-    {
-        Name = "Energy";
-    }
-}
-
-public interface IWalletView
-{
-    
+    public CurrencyType type;
+    public Sprite sprite;
 }
